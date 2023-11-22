7913c01cbe307e28ff5f03d1fc10f8f6adcd5218
amir <amir.khaledmohamed2003@gmail.com>
2023-11-23 00:39:13 +0200

    m7adeshhhhhh yl3b fe el mainnnnnnnnn

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import helpers.Authentication;
 import models.Admin;
+import models.Lecturer;
 import models.User;
 
 import java.util.Locale;
@@ -33,18 +34,65 @@ public class Main {
                 System.out.println("Enter Password: ");
                 password = input.nextLine().toLowerCase();
 
-                if (Authentication.adminLogin("userName", "password"))
-                {
+                if (Authentication.adminLogin("userName", "password")) {
                     System.out.println("login Success");
                     System.out.println("---------------------------\n");
+
+                    Admin admin1 = new Admin(username, password, "Admin");
 
                     System.out.println("choose only on option to manage: ");
 
                     System.out.println("1=> Lecturer\n2=> Student\n3=> Subject");
                     System.out.print("Enter your answer");
-                    String optionsAnswer = input.nextLine()
-                }
+                    String optionsAnswer = input.nextLine();
 
+                    if (optionsAnswer.equals("1")) {
+                        int op;
+                        int lecID;
+                        do {
+
+                            System.out.println("Select operation");
+                            System.out.println("1=> add\n2=> delete\n3=>search\n=>list\n5=>update\n0=> exit");
+                            System.out.print("enter operation num: ");
+                            op = input.nextInt();
+                            input.nextLine();
+
+                            switch (op) {
+                                case 1:
+                                    System.out.print("Enter lecturer Username: ");
+                                    String LecUsername = input.nextLine();
+
+                                    System.out.print("Enter lecturer password: ");
+                                    String LecPassword = input.nextLine();
+
+
+                                    admin1.lectureManager.addLecturer(new Lecturer(LecUsername, LecPassword, "Lecturer"));
+                                    System.out.println("---- lecturer added successfully ----");
+                                    break;
+
+                                case 2:
+                                    System.out.print("Enter lecturer id to delete: ");
+                                    lecID = input.nextInt();
+                                    input.nextLine();
+
+                                    System.out.println(admin1.lectureManager.deleteLecturer(lecID) ? "deleted success" : "failure");
+                                    break;
+
+                                case 3:
+                                    System.out.print("Enter lecturer id to search: ");
+                                    lecID = input.nextInt();
+
+                                   if( admin1.lectureManager.searchLecturer(lecID) == 0)
+                                   {
+                                       System.out.println("the lecturer not found");
+                                   }
+
+
+                            }
+
+                        } while (op != 0);
+                    }
+                }
 
 
             }
