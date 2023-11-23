eb79b9e01102d645027183856c438f1559b19f67
rania <lali92200@gmail.com>
2023-11-23 17:12:03 +0200

    Student management

---
+++ Can_Update/Java_Project/src/java_project/Student.java
@@ -0,0 +1,31 @@
+package java_project;
+
+import java.util.ArrayList;
+
+public class Student {
+
+
+private final int id;
+
+public int getID() {
+        return id;
+    }
+
+
+
+
+    public static void add(ArrayList<Student> students) {
+    }
+
+    public static void remove(ArrayList<Student> students) {
+    }
+
+
+
+
+
+    public static ArrayList<Student> get(int index) {
+        return null;
+    }
+    
+}
