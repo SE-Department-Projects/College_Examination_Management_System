97071bf3484fd6c82b117fce73a34c6229791e87
David Yousry <davidyousry45@gmail.com>
2023-11-23 16:19:16 +0200

    added id to subj

--- src/models/Subject.java
+++ src/models/Subject.java
@@ -1,11 +1,17 @@
 package models;
 
 public class Subject {
+    private int SubjID;
     private String name;
     private int LecturerID;
 
-    Subject(String name, int LecturerID){
+    Subject(int SubjID, String name, int LecturerID){
+        this.SubjID = SubjID;
         this.name = name;
         this.LecturerID = LecturerID;
+    }
+
+    public int getSubjID() {
+        return SubjID;
     }
 }
