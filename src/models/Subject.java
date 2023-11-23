d2459041e4214c4cdd27593074547fdca84e30fd
David Yousry <davidyousry45@gmail.com>
2023-11-23 13:25:10 +0200

    added subject class (not finished)

---
+++ src/models/Subject.java
@@ -0,0 +1,11 @@
+package models;
+
+public class Subject {
+    private String name;
+    private int LecturerID;
+
+    Subject(String name, int LecturerID){
+        this.name = name;
+        this.LecturerID = LecturerID;
+    }
+}
