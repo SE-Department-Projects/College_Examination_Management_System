dbc356b4bccae14d180b9c54e5c57aab380708d8
David Yousry <davidyousry45@gmail.com>
2023-11-25 20:50:45 +0200

    folder2ot

--- src/folder2ot/Exam.java
+++ src/folder2ot/Exam.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package java_project;
+package folder2ot;
 
 import java.util.*;
 
--- src/folder2ot/ExamManager.java
+++ src/folder2ot/ExamManager.java
@@ -2,7 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package java_project;
+
+package folder2ot;
 
 import java.util.*;
 
