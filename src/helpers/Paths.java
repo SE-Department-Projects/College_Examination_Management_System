5a43c5f462c47ed2ff6ecbebecbf6e22b05d3bec
amir <amir.khaledmohamed2003@gmail.com>
2023-12-10 19:01:21 +0200

    test

--- src/helpers/Paths.java
+++ src/helpers/Paths.java
@@ -3,7 +3,7 @@ package helpers;
 // the paths for all the files
 public class Paths {
 
-    static private final String base = "src/Files";  // if there is an error delete src/
+    static private final String base = "Files";  // if there is an error delete src/
 
     static public String adminPath = base + "/admin.txt";
 
